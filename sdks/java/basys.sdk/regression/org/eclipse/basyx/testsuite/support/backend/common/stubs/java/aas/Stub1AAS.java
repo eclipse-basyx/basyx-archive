a191693c16f8cff94e5428d9cecb3789491c1fa1
tkuhn1l6 <thomas.kuhn@iese.fraunhofer.de>
2018-09-26 16:11:44 +0200

    Testsuite Bugfix (2)

--- sdks/java/basys.sdk/regression/org/eclipse/basyx/testsuite/support/backend/common/stubs/java/aas/Stub1AAS.java
+++ sdks/java/basys.sdk/regression/org/eclipse/basyx/testsuite/support/backend/common/stubs/java/aas/Stub1AAS.java
@@ -2,6 +2,7 @@ package org.eclipse.basyx.testsuite.support.backend.common.stubs.java.aas;
 
 import org.eclipse.basyx.aas.impl.resources.basic.AssetAdministrationShell;
 import org.eclipse.basyx.aas.impl.resources.basic.AssetKind;
+import org.eclipse.basyx.aas.impl.resources.basic.ReferencedSubModel;
 
 
 
@@ -24,5 +25,26 @@ public class Stub1AAS extends AssetAdministrationShell {
 	    setAssetKind(AssetKind.INSTANCE);
 	    setAssetTypeDefinition("assetTypeDefinition");
 	    setDisplayName("displayName");	
+	    
+	    
+	    // Register sub models
+	    
+	    // - Sub model statusSM is a referenced sub model. Its contents are provided by a model provider. 
+	    //   We do not know its exact internal structure here, since some properties (with varying cardinalities) are unknown. 
+	    ReferencedSubModel statusSM = new ReferencedSubModel();
+	    statusSM.setAssetKind(AssetKind.INSTANCE);
+	    statusSM.setName("statusSM");
+	    statusSM.setId("statusSM");
+	    //statusSM.setTypeDefinition("smType");
+	    this.addSubModel(statusSM);
+
+	    // - Sub model Stub2SM is a referenced sub model. Its contents are provided by a model provider. 
+	    //   We do not know its exact internal structure here, since some properties (with varying cardinalities) are unknown. 
+	    ReferencedSubModel stub2SM = new ReferencedSubModel();
+	    stub2SM.setAssetKind(AssetKind.INSTANCE);
+	    stub2SM.setName("Stub2SM");
+	    stub2SM.setId("Stub2SM");
+	    //statusSM.setTypeDefinition("smType");
+	    this.addSubModel(stub2SM);
 	}
 }
