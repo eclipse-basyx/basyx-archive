aac8478bb8144a999408ab319f26e85f154fa773
Thomas Psota <thomas.psota@iese.fraunhofer.de>
2019-05-14 11:37:27 +0200

    Added JSON serialization for basyx::any
    Change-Id: If0bfc340e6ec5093a60c1cb841284370030747fc
    Signed-off-by: Thomas Psota <thomas.psota@iese.fraunhofer.de>

---
+++ sdks/c++/basys.sdk.cc/src/basyx/json/typeid.cpp
@@ -0,0 +1,10 @@
+#include "typeid.h"
+
+
+
+constexpr char basyx::serialization::basysType<bool>::string[];
+constexpr char basyx::serialization::basysType<int>::string[];
+constexpr char basyx::serialization::basysType<float>::string[];
+constexpr char basyx::serialization::basysType<double>::string[];
+constexpr char basyx::serialization::basysType<char>::string[];
+constexpr char basyx::serialization::basysType<std::string>::string[];
