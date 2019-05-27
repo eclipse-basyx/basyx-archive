a0569ff1b4c7073063a1873402a0148466a02170
Patrick Pschorn <patrick.pschorn@iese.fraunhofer.de>
2019-05-27 14:58:53 +0200

    Modify References
    - adds single key constructor
    - removes duplicate ReferenceElement
    
    
    Signed-off-by: Patrick Pschorn <patrick.pschorn@iese.fraunhofer.de>

--- sdks/java/basys.sdk/src/org/eclipse/basyx/aas/metamodel/hashmap/aas/submodelelement/ReferenceElement.java
+++ sdks/java/basys.sdk/src/org/eclipse/basyx/aas/metamodel/hashmap/aas/submodelelement/ReferenceElement.java
@@ -1,35 +1,36 @@
 package org.eclipse.basyx.aas.metamodel.hashmap.aas.submodelelement;
 
-import java.util.HashMap;
-
 import org.eclipse.basyx.aas.api.metamodel.aas.reference.IReference;
-import org.eclipse.basyx.aas.api.metamodel.aas.submodelelement.IReferenceElement;
+import org.eclipse.basyx.aas.api.metamodel.aas.submodelelement.property.IReferenceElement;
 import org.eclipse.basyx.aas.metamodel.facades.ReferenceElementFacade;
 import org.eclipse.basyx.aas.metamodel.hashmap.aas.reference.Reference;
 
 /**
- * ReferenceElement as described by DAAS document <br/>
+ * A ReferenceElement as defined in DAAS document <br/>
  * A reference element is a data element that defines a reference to another
  * element within the same or another AAS or a reference to an external object
  * or entity.
  * 
- * @author schnicke
+ * @author pschorn, schnicke
  *
  */
-public class ReferenceElement extends HashMap<String, Object> implements IReferenceElement {
+public class ReferenceElement extends DataElement implements IReferenceElement {
+
 	private static final long serialVersionUID = 1L;
 	
 	public static final String VALUE="value";
 
+	/**
+	 * Constructor
+	 */
 	public ReferenceElement() {
 		put(VALUE, null);
 	}
 
 	/**
-	 * 
 	 * @param ref
-	 *            Reference to any other referable element of the same of any other
-	 *            AAS or a reference to an external object or entity.
+	 *            Reference to any other referable element of the same or any other
+	 *            AAS or a reference to an external object or entity
 	 */
 	public ReferenceElement(Reference ref) {
 		put(VALUE, ref);
@@ -37,12 +38,13 @@ public class ReferenceElement extends HashMap<String, Object> implements IRefere
 
 	@Override
 	public void setValue(IReference ref) {
-		new ReferenceElementFacade(this).setValue(ref);
+	new ReferenceElementFacade(this).setValue(ref);
 		
 	}
 
 	@Override
 	public IReference getValue() {
-		return new ReferenceElementFacade(this).getValue();
+	return new ReferenceElementFacade(this).getValue();
 	}
+
 }
