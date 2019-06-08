240e2cde5df89686386ff06eee64a1b0c4aa0df9
tkuhn1l6 <thomas.kuhn@iese.fraunhofer.de>
2019-06-09 00:47:07 +0200

    Added HTTP tools (dynamic get/set properties) for active AAS

---
+++ components/basys.components/src/org/eclipse/basyx/tools/aas/active/HTTPGetter.java
@@ -0,0 +1,55 @@
+package org.eclipse.basyx.tools.aas.active;
+
+import java.io.Serializable;
+import java.util.function.Supplier;
+
+import org.eclipse.basyx.tools.webserviceclient.WebServiceRawClient;
+
+
+
+
+/**
+ * Implement a getter function that queries the value from a HTTP server. Expects a string response.
+ * 
+ * @author kuhn
+ *
+ */
+public class HTTPGetter implements Supplier<Object>, Serializable {
+
+	
+	/**
+	 * Version number of serialized instances
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	/**
+	 * URL of server that provides the requested information
+	 */
+	protected String serverURL = null;
+	
+	
+
+	/**
+	 * Constructor
+	 */
+	public HTTPGetter(String url) {
+		// Store URL
+		serverURL = url;
+	}
+	
+	
+	
+	/**
+	 * Return value
+	 */
+	@Override
+	public Object get() {
+		// Create web service client
+		WebServiceRawClient rawClient = new WebServiceRawClient();
+		
+		// Delegate call to WebService RAW client
+		return rawClient.get(serverURL);
+	}
+}
+
---
+++ components/basys.components/src/org/eclipse/basyx/tools/aas/active/HTTPSupplier.java
@@ -0,0 +1,54 @@
+package org.eclipse.basyx.tools.aas.active;
+
+import java.io.Serializable;
+import java.util.function.Consumer;
+import org.eclipse.basyx.tools.webserviceclient.WebServiceRawClient;
+
+
+
+
+/**
+ * Implement a setter (supplier) function that posts a value to a HTTP server.
+ * 
+ * @author kuhn
+ *
+ */
+public class HTTPSupplier implements Consumer<String>, Serializable {
+
+	
+	/**
+	 * Version number of serialized instances
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	/**
+	 * URL of server that provides the requested information
+	 */
+	protected String serverURL = null;
+	
+	
+
+	/**
+	 * Constructor
+	 */
+	public HTTPSupplier(String url) {
+		// Store URL
+		serverURL = url;
+	}
+	
+	
+	
+	/**
+	 * Return value
+	 */
+	@Override
+	public void accept(String value) {
+		// Create web service client
+		WebServiceRawClient rawClient = new WebServiceRawClient();
+		
+		// Delegate call to WebService RAW client
+		rawClient.post(serverURL, value);
+	}
+}
+
