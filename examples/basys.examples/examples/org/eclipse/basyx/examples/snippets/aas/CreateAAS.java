d36135d83a766bb4680383214e3f0369dd459c08
Frank Schnicke <frank.schnicke@iese.fraunhofer.de>
2019-07-08 16:05:59 +0200

    Removes obsolete code
    Change-Id: Id07654c58412f58a27007537595fe8c8222d666a
    Signed-off-by: Frank Schnicke <frank.schnicke@iese.fraunhofer.de>

--- examples/basys.examples/examples/org/eclipse/basyx/examples/snippets/aas/CreateAAS.java
+++ examples/basys.examples/examples/org/eclipse/basyx/examples/snippets/aas/CreateAAS.java
@@ -1,24 +1,14 @@
 package org.eclipse.basyx.examples.snippets.aas;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
-import org.eclipse.basyx.aas.backend.connector.http.HTTPConnectorProvider;
 import org.eclipse.basyx.aas.metamodel.hashmap.aas.AssetAdministrationShell;
-import org.eclipse.basyx.components.servlet.submodel.AASServlet;
-import org.eclipse.basyx.examples.contexts.BaSyxExamplesContext_Empty;
-import org.eclipse.basyx.examples.deployment.BaSyxDeployment;
-import org.eclipse.basyx.examples.support.directory.ExamplesPreconfiguredDirectory;
-import org.eclipse.basyx.vab.core.VABConnectionManager;
-import org.junit.ClassRule;
 import org.junit.Test;
 
 
 
 /**
  * Code snippet that illustrates the creation of an Asset Administration Shell (AAS) by extending an SDK class
- * 
- * The AAS is deployed to an AASServlet instance running on a Apache Tomcat HTTP server. The AASServlet exports
- * an Asset Administration Shell via the defined BaSyx REST interface. 
  * 
  * @author kuhn
  *
@@ -44,43 +34,6 @@ public class CreateAAS {
 			setId("aas-001");
 		}
 	}
-
-	
-	
-	/**
-	 * Create VAB connection manager backend
-	 * 
-	 * The connection manager uses a preconfigured directory for resolving IDs to 
-	 * network addresses, and a HTTP connector to connect to VAB objects.
-	 */
-	protected VABConnectionManager connManager = new VABConnectionManager(
-			// Add example specific mappings
-			new ExamplesPreconfiguredDirectory()
-			    // - SDK connectors encapsulate relative path Asset Administration Shell
-				.addMapping("aas-001",    "http://localhost:8080/basys.examples/Testsuite/components/BaSys/1.0/SampleAAS"),
-			// We connect via HTTP
-			new HTTPConnectorProvider());
-
-	
-	
-	/**
-	 * The BaSyx Deployment instantiates and starts context elements for this example. 
-	 * 
-	 * This example instantiates the BaSyxExamplesContext_1MemoryAASServer_1SQLDirectory
-	 * example context that creates one AAS server, and one SQL based AAS registry.
-	 * 
-	 * BaSyxDeployment contexts instantiate all components on the IP address of the host. 
-	 * Therefore, all components use the same IP address. 
-	 */
-	@ClassRule
-	public static BaSyxDeployment context = new BaSyxDeployment(
-				// Servlets for example snippet
-				new BaSyxExamplesContext_Empty().
-					// Deploy example specific servlets to Tomcat server in this context
-					addServletMapping("/Testsuite/components/BaSys/1.0/SampleAAS/*",         new AASServlet(new ExampleAssetAdministrationShell()))
-			);
-
-	
 	
 	/**
 	 * Run code snippet. Connect to AAS on server, access AAS properties. 
@@ -95,7 +48,7 @@ public class CreateAAS {
 		
 		
 		// Check result
-		assertTrue(propertyId.equals("aas-001"));
+		assertEquals("aas-001", propertyId);
 	}
 }
 
