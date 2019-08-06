abdef5cf8d2307fd5e5e6f2857ce5557ffd48d1d
Patrick Pschorn <patrick.pschorn@iese.fraunhofer.de>
2019-08-06 18:09:53 +0200

    Creates example project for a dockerized directory service
    * Start the SQLDirectoryServlet and an initialized Postgres in Docker
    containers
    * Demonstrates Maven Docker integration
    * Demonstrates use of docker-compose script to manage containers
    * Beware: After containers are shut down, the database state will be
    lost! -> Make permanent for production use.
    
    Signed-off-by: Patrick Pschorn <patrick.pschorn@iese.fraunhofer.de>

---
+++ examples/basys.registry/src/main/java/registry/CustomSQLDirectoryServlet.java
@@ -0,0 +1,40 @@
+package main.java.registry;
+
+
+import org.eclipse.basyx.components.servlets.SQLDirectoryServlet;
+
+
+/**
+ * SQL database based directory provider
+ * 
+ * This directory provider provides a static directory. It therefore only
+ * supports get() operations. Modification of the directory via
+ * PUT/POST/PATCH/DELETE operations is not supported.
+ * 
+ * @author kuhn, pschorn
+ *
+ */
+public class CustomSQLDirectoryServlet extends SQLDirectoryServlet {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Path to the directory.properties file, that contains config data for the SQL
+	 * connection
+	 */
+	private static String configFilePath = "/basys/directory.properties";
+
+
+	/**
+	 * Provide HTTP interface with JSONProvider to handle serialization and
+	 * SQLDirectoryProvider as backend
+	 */
+	public CustomSQLDirectoryServlet() {
+		super(configFilePath);
+
+	}
+
+	
+}
+
---
+++ examples/basys.registry/src/test/java/TestDirectorySQLProviderExtension.java
@@ -0,0 +1,29 @@
+package test.java;
+
+import org.eclipse.basyx.regression.directory.sql.TestDirectorySQLProvider;
+
+/**
+ * Test queries to SQL directory provider. Tomcat server and postgres db is
+ * started by docker compose
+ * 
+ * @author kuhn, ps
+ *
+ */
+public class TestDirectorySQLProviderExtension extends TestDirectorySQLProvider {
+
+	// Directory web service URL
+	@Override
+	public void setUp() {
+		this.wsURL = "http://localhost:8081/registry";
+
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public TestDirectorySQLProviderExtension() {
+		super();
+	}
+}
