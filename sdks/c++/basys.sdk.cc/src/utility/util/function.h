1f96b42fd889ba04074365f1b944c42307ab9e34
Thomas Psota <thomas.psota@iese.fraunhofer.de>
2019-08-21 09:51:15 +0200

    Added missing serialization for basyx::function
    Change-Id: If31e7fb4ca9db8f4ae9ea4ef56139e369dce0316
    Signed-off-by: Thomas Psota <thomas.psota@iese.fraunhofer.de>

--- sdks/c++/basys.sdk.cc/src/utility/util/function.h
+++ sdks/c++/basys.sdk.cc/src/utility/util/function.h
@@ -12,8 +12,12 @@
 #include <util/any.h>
 #include <util/util.h>
 
+#include <json/json.hpp>
+
 namespace basyx
 {
+	using json_t = nlohmann::json;
+
 	class function_base
 	{
 	public:
@@ -22,40 +26,6 @@ namespace basyx
 			return nullptr;
 		};
 	};
-
-//	template<typename RetType, typename... Params>
-//	class stdfunction : public function_base
-//	{
-//	public:
-////		using function_t = RetType(*)(Params...);
-//		using function_t = std::function<RetType(Params...)>;
-//	private:
-//		function_t func;
-//
-//		template<typename F, std::size_t... Is>
-//		static RetType invoke_helper(F f, std::vector<basyx::any> & params, util::index_sequence<Is...>)
-//		{
-//			return f(params[Is].Get<Params>()...);
-//		};
-//	public:
-//		template<typename F>
-//		stdfunction(F f)
-//		{
-//			func = f;
-//		};
-//
-//		template<typename Indices = util::make_index_sequence<sizeof...(Params)>>
-//		RetType invoke(std::vector<basyx::any> & params)
-//		{
-//			return invoke_helper(func, params, Indices{});
-//		};
-//
-//		virtual basyx::any invoke_any(std::vector<basyx::any> & params) override
-//		{
-//			return invoke(params);
-//		};
-//	};
-
 
 	template<typename RetType, typename... Params>
 	class function : public function_base
@@ -87,7 +57,14 @@ namespace basyx
 		{
 			return invoke(params);
 		};
+
 	};
+
+	template<typename RetType, typename... Params>
+	static void to_json(json_t & j, const function<RetType, Params...> & f) {
+		j = json_t{ {"_basyxFunctionType", "operation" } };
+	}
+
 
 	template<typename Object, typename RetType, typename... Params>
 	function<RetType, Params...> make_function(RetType(Object::* f)(Params...), Object * o)
