451cc18a36fb23232dd2c3986f5ccd266606ba94
Johannes Wendel <johannes.wendel@iese.fraunhofer.de>
2019-09-12 12:43:00 +0200

    Finished tests for ConnectedProperty.
    Change-Id: I44023355dffb6f073874a36d35011e59a403027c
    Signed-off-by: Johannes Wendel <johannes.wendel@iese.fraunhofer.de>

--- sdks/c++/basys.sdk.cc/tests/regression/aas/submodelelement/property/connected/test_ConnectedProperty.cpp
+++ sdks/c++/basys.sdk.cc/tests/regression/aas/submodelelement/property/connected/test_ConnectedProperty.cpp
@@ -8,20 +8,132 @@
 
 #include "aas/submodelelement/property/connected/ConnectedProperty.h"
 #include "vab/core/proxy/IVABElementProxy.h"
+#include "support/VABProxyMock.cpp" 
+#include "basyx/anyTypeChecker.h"
 
 using namespace basyx::aas::submodelelement::property;
+using namespace basyx::vab::core::proxy;
 
 class ConnectedPropertyTest : public ::testing::Test
 {
-  std::shared_ptr<basyx::vab::core::proxy::IVABElementProxy> proxy;
+public:
+  std::shared_ptr<IVABElementProxy> proxy;
+  std::shared_ptr<mockups::VABProxyMock> mock;
 protected:
   void SetUp() override
   {
+    mock = std::make_shared<mockups::VABProxyMock>();
+    proxy = mock;
   }
 };
 
-TEST_F(ConnectedPropertyTest, TestReadElementValueTest)
+TEST_F(ConnectedPropertyTest, TestGetPropertyType)
 {
-  auto property = std::make_shared<IProperty>(ConnectedProperty(PropertyType::Collection, proxy));
+  std::shared_ptr<IProperty> property(new ConnectedProperty(PropertyType::Collection, proxy));
+  
+  ASSERT_EQ(PropertyType::Collection, property->getPropertyType());
+  ASSERT_EQ(0, mock->overallMockCalls());
+}
 
+TEST_F(ConnectedPropertyTest, TestSetValueString)
+{
+  std::shared_ptr<IProperty> property(new ConnectedProperty(PropertyType::Collection, proxy));
+
+  basyx::any any = std::string("this is a string");
+
+  property->setValue(any);
+
+  auto calling_parameters = mock->updateElementCallValues;
+  
+  ASSERT_EQ(calling_parameters.at(0).first, PropertyPaths::VALUE);
+  ASSERT_EQ(calling_parameters.at(0).second.Get<std::string>(), any.Get<std::string>());
+  ASSERT_EQ(calling_parameters.at(1).first, PropertyPaths::VALUETYPE);
+  ASSERT_EQ(calling_parameters.at(1).second.Get<std::string>(), basyx::PropertyTypeInfo::STRING);
+  ASSERT_EQ(2, mock->overallMockCalls());
+}
+
+TEST_F(ConnectedPropertyTest, TestSetValueInt)
+{
+  std::shared_ptr<IProperty> property(new ConnectedProperty(PropertyType::Collection, proxy));
+
+  basyx::any any = 235;
+
+  property->setValue(any);
+
+  auto calling_parameters = mock->updateElementCallValues;
+
+  ASSERT_EQ(calling_parameters.at(0).first, PropertyPaths::VALUE);
+  ASSERT_EQ(calling_parameters.at(0).second.Get<int>(), any.Get<int>());
+  ASSERT_EQ(calling_parameters.at(1).first, PropertyPaths::VALUETYPE);
+  ASSERT_EQ(calling_parameters.at(1).second.Get<std::string>(), basyx::PropertyTypeInfo::INT);
+  ASSERT_EQ(2, mock->overallMockCalls());
+}
+
+TEST_F(ConnectedPropertyTest, TestGetValue)
+{
+  std::shared_ptr<IProperty> property(new ConnectedProperty(PropertyType::Collection, proxy));
+
+  auto value = property->getValue();
+
+  ASSERT_EQ(value.Get<std::string>(), "called with " + std::string(PropertyPaths::VALUE));
+  ASSERT_EQ(1, mock->overallMockCalls());
+}
+
+TEST_F(ConnectedPropertyTest, TestSetValueID)
+{
+  std::shared_ptr<IProperty> property(new ConnectedProperty(PropertyType::Collection, proxy));
+
+  basyx::any any = std::string("ID");
+  property->setValueId(any);
+
+  auto calling_parameters = mock->updateElementCallValues;
+
+  ASSERT_EQ(calling_parameters.at(0).first, PropertyPaths::VALUEID);
+  ASSERT_EQ(calling_parameters.at(0).second.Get<std::string>(), any.Get<std::string>());
+  ASSERT_EQ(1, mock->overallMockCalls());
+}
+
+TEST_F(ConnectedPropertyTest, TestGetValueID)
+{
+  std::shared_ptr<IProperty> property(new ConnectedProperty(PropertyType::Collection, proxy));
+
+  auto any = property->getValueId();
+
+  ASSERT_EQ(any.Get<std::string>(), "called with " + std::string(PropertyPaths::VALUEID));
+  ASSERT_EQ(1, mock->overallMockCalls());
+}
+
+TEST_F(ConnectedPropertyTest, TestGetID)
+{
+  std::shared_ptr<IProperty> property(new ConnectedProperty(PropertyType::Collection, proxy));
+
+  auto id = property->getId();
+
+  //Further tests can be found in ConnectedDataElementTests
+  ASSERT_EQ(id, "called with " + std::string(basyx::aas::qualifier::ReferablePaths::IDSHORT));
+  ASSERT_EQ(1, mock->overallMockCalls());
+}
+
+TEST_F(ConnectedPropertyTest, TestSetID)
+{
+  std::shared_ptr<IProperty> property(new ConnectedProperty(PropertyType::Collection, proxy));
+
+  property->setId("ID");
+
+  ASSERT_EQ(property->getId(), "called with " + std::string(basyx::aas::qualifier::ReferablePaths::IDSHORT));
+  ASSERT_EQ(1, mock->updateElementValue_calls);
+  ASSERT_EQ(1, mock->readElementValue_calls);
+  ASSERT_EQ(2, mock->overallMockCalls());
+}
+
+TEST_F(ConnectedPropertyTest, TestSetID)
+{
+  std::shared_ptr<IProperty> property(new ConnectedProperty(PropertyType::Collection, proxy));
+
+  property->setId("ID");
+
+  ASSERT_EQ(property->getId(), "called with " + std::string(basyx::aas::qualifier::ReferablePaths::IDSHORT));
+  ASSERT_EQ(1, mock->updateElementValue_calls);
+  ASSERT_EQ(1, mock->readElementValue_calls);
+  ASSERT_EQ(2, mock->overallMockCalls());
 }
---
+++ sdks/c++/basys.sdk.cc/tests/regression/aas/support/VABProxyMock.cpp
@@ -0,0 +1,72 @@
+/*
+ * VABProxyMock.cpp
+ *
+ *      Author: wendel
+ */
+
+#include "vab/core/proxy/IVABElementProxy.h"
+
+namespace basyx {
+namespace vab {
+namespace core {
+namespace proxy {
+namespace mockups {
+
+class VABProxyMock : public IVABElementProxy
+{
+public:
+  virtual basyx::any readElementValue(const VABPath & elementPath) override
+  {
+    this->readElementValue_calls++;
+
+    return basyx::any("called with " + elementPath.toString());
+  }
+
+  virtual void updateElementValue(const VABPath & elementPath, const basyx::any & newValue) override
+  {
+    this->updateElementValue_calls++;
+
+    this->updateElementCallValues.push_back(std::make_pair(elementPath.toString(), newValue));
+  }
+
+  virtual void createElement(const VABPath & elementPath, const basyx::any & newValue) override
+  {
+    this->createElement_calls++;
+  }
+
+  virtual void deleteElement(const VABPath & elementPath) override
+  {
+    this->deleteElement_calls++;
+  }
+
+  virtual void deleteElement(const VABPath & elementPath, const basyx::any & value) override
+  {
+    this->deleteElement2_calls++;
+  }
+
+  virtual basyx::any invoke(const VABPath & elementPath, basyx::objectCollection_t & parameter) override
+  {
+    this->invoke_calls++;
+    return basyx::any();
+  }
+
+  int overallMockCalls()
+  {
+    return readElementValue_calls + updateElementValue_calls + createElement_calls + deleteElement_calls + deleteElement2_calls + invoke_calls;
+  }
+
+  int readElementValue_calls = 0;
+  int updateElementValue_calls = 0;
+  int createElement_calls = 0;
+  int deleteElement_calls = 0;
+  int deleteElement2_calls = 0;
+  int invoke_calls = 0;
+
+  std::vector<std::pair<std::string, basyx::any>> updateElementCallValues;
+};
+
+}
+}
+}
+}
+}
