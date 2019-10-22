bdf2e76eb154a385727e945c521fb123b60ed861
Johannes Wendel <johannes.wendel@iese.fraunhofer.de>
2019-10-22 15:02:10 +0200

    Adds ConnectedOperation with appropriate tests.
    Change-Id: I94e3a271f7fb911d01a862dda2e1ed9cc3652d24
    Signed-off-by: Johannes Wendel <johannes.wendel@iese.fraunhofer.de>

--- sdks/c++/basys.sdk.cc/tests/regression/aas/backend/connected/submodelelement/operation/test_ConnectedOperation.cpp
+++ sdks/c++/basys.sdk.cc/tests/regression/aas/backend/connected/submodelelement/operation/test_ConnectedOperation.cpp
@@ -6,7 +6,8 @@
 
 #include <gtest/gtest.h>
 
-#include "backend/connected/aas/submodelelement/ConnectedRelationshipElement.h"
+#include "aas/submodelelement/operation/IOperation.h"
+#include "backend/connected/aas/submodelelement/operation/ConnectedOperation.h"
 #include "vab/core/proxy/IVABElementProxy.h"
 #include "support/VABProxyMock.cpp"
 
@@ -18,24 +19,53 @@ class ConnectedOperationTest : public ::testing::Test
 {
 public:
   std::shared_ptr<IVABElementProxy> proxy;
-  std::shared_ptr<mockups::VABProxyMock> mock;
+  std::shared_ptr<mockups::VABProxyMockCollection> mock;
 protected:
   void SetUp() override
   {
-    mock = std::make_shared<mockups::VABProxyMock>();
+    mock = std::make_shared<mockups::VABProxyMockCollection>();
     proxy = mock;
   }
 };
 
-TEST_F(ConnectedOperationTest, TestGetFirst)
+TEST_F(ConnectedOperationTest, TestGetParameterTypes)
 {
   std::shared_ptr<basyx::objectMap_t> map(new basyx::objectMap_t);
 
-  ConnectedRelationshipElement connected_element(proxy);
+  ConnectedOperation connected_operation(proxy);
 
-  auto first = connected_element.getFirst();
+  auto types = connected_operation.getParameterTypes();
 
-  ASSERT_EQ(std::string(submodelelement::RelationshipElementPath::FIRST), mock->getElementCallValues.at(0));
+  ASSERT_EQ(submodelelement::operation::OperationPaths::INPUT, mock->getElementCallValues.at(0));
+  ASSERT_EQ(1, mock->overallMockCalls());
+}
+
+TEST_F(ConnectedOperationTest, TestGetReturnTypes)
+{
+  std::shared_ptr<basyx::objectMap_t> map(new basyx::objectMap_t);
+
+  ConnectedOperation connected_operation(proxy);
+
+  auto types = connected_operation.getReturnTypes();
+
+  ASSERT_EQ(submodelelement::operation::OperationPaths::OUTPUT, mock->getElementCallValues.at(0));
+  ASSERT_EQ(1, mock->overallMockCalls());
+}
+
+TEST_F(ConnectedOperationTest, TestInvoke)
+{
+  std::shared_ptr<basyx::objectMap_t> map(new basyx::objectMap_t);
+
+  ConnectedOperation connected_operation(proxy);
+
+  basyx::objectCollection_t collection;
+  collection.push_back(basyx::any(2));
+
+  auto invoked = connected_operation.invoke(collection);
+
+  ASSERT_EQ("called with ", invoked.Get<std::string>());
+  ASSERT_EQ(1, mock->invokeCallParameter.size());
+  ASSERT_EQ(2, mock->invokeCallParameter.at(0).Get<int>());
   ASSERT_EQ(1, mock->overallMockCalls());
 }
 
