17c2db43369f8fd07a97ebbc70ed1ab1e38dc0a1
Thomas Psota <thomas.psota@iese.fraunhofer.de>
2019-12-04 23:15:14 +0100

    Activated continuous integration for C++ SDK
    Change-Id: I65a00b1615efd56223ff14fde8ba4d6b567cb8d7
    Signed-off-by: Thomas Psota <thomas.psota@iese.fraunhofer.de>

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -1,20 +1,22 @@
-#!/bin/sh
-
-##
+#!/bin/bash
+set -e
 
 CWD=$(pwd)
 echo "CWD: $CWD"
 
-GIT_DIFF=$(/usr/bin/git diff --name-only origin/CI_Test)
+GIT_DIFF=$(/usr/bin/git diff --name-only origin/master)
 
-CPP_SDK_CHANGED=$(echo $GIT_DIFF | grep ".*/sdks/c++/.*" | wc -l)
+CPP_SDK_CHANGED=$(echo $GIT_DIFF | grep -e ".*/basys\.sdk\.cc/.*" | wc -l)
 
-#if [ $((CPP_SDK_CHANGED > 0)) ];
-#then
+if  ((CPP_SDK_CHANGED > 0));
+then
     echo "Building and testing BaSyx C++ SDK"
 
     mkdir build && cd build
-    cmake ../sdks/c++/basys.sdk.cc -DBASYX_UTILITY_PROJECTS=OFF
-    make -j4 all
+    cmake ../sdks/c++/basys.sdk.cc -DBASYX_UTILITY_PROJECTS=OFF -DBUILD_SHARED_LIBS=ON
+    make all
     ctest
-#fi
+else
+    echo "No files changed in C++ SDK."
+    echo "Skipping continous integration tests in C++ SDK."
+fi
