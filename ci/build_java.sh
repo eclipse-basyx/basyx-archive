768032b316aaf6ade218eeec794efb9660aae68d
Daniel Espen <daniel.espen@iese.fraunhofer.de>
2020-02-10 10:23:46 +0100

    Fixes CI detection of changed Java files
    Change-Id: Ib6da17b48a56098e52e1518771d4948e2ed6165a
    Signed-off-by: Daniel Espen <daniel.espen@iese.fraunhofer.de>

--- ci/build_java.sh
+++ ci/build_java.sh
@@ -7,21 +7,21 @@ MVN="mvn -ntp -Duser.home=/home/jenkins/agent"
 
 GIT_DIFF=$(/usr/bin/git diff-tree --no-commit-id --name-only -r HEAD)
 
-JAVA_SDK_CHANGED=$(echo $GIT_DIFF | grep ".*sdks/java/.*" | wc -l)
+JAVA_SDK_CHANGED=$(echo $GIT_DIFF | grep -e "sdks/java/basys.sdk/" -e "components/basys.components/" -e "examples/basys.examples/" | wc -l)
 
 if ((JAVA_SDK_CHANGED > 0));
 then
     cd ./sdks/java/basys.sdk
     $MVN clean install
-    cd "$CWD"
+    cd ../../../
 
     cd ./components/basys.components
     $MVN clean install
-    cd "$CWD"
+    cd ../../
 
     cd ./examples/basys.examples
     $MVN verify
-    cd "$CWD"
+    cd ../../
 else
     echo "No files changed in Java SDK."
     echo "Skipping continous integration tests for Java."
