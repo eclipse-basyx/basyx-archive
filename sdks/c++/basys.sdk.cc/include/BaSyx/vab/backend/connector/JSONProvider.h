e7e46c3cf7464a926071270523d3113930eaa6ef
Thomas Psota <thomas.psota@iese.fraunhofer.de>
2020-03-27 11:05:15 +0100

    Removed frozen and clock settings from VAB
    Change-Id: I351750b7d427ca9453249e259f80298a5778a6b2
    Signed-off-by: Thomas Psota <thomas.psota@iese.fraunhofer.de>

--- sdks/c++/basys.sdk.cc/include/BaSyx/vab/backend/connector/JSONProvider.h
+++ sdks/c++/basys.sdk.cc/include/BaSyx/vab/backend/connector/JSONProvider.h
@@ -39,11 +39,8 @@ public:
 
     std::string processBaSysSet(std::string const& path, std::string const& serializedJSONValue)
     {
-        if (!isFrozen(BaSysID::getAddress(path))) {
-            auto deserialized = basyx::serialization::json::deserialize(serializedJSONValue);
-            providerBackend->setModelPropertyValue(path, std::move(deserialized));
-            incrementClock(BaSysID::getAddress(path));
-        }
+        auto deserialized = basyx::serialization::json::deserialize(serializedJSONValue);
+        providerBackend->setModelPropertyValue(path, std::move(deserialized));
 
         return serializeSuccess();
     }
@@ -58,41 +55,23 @@ public:
 
     std::string processBaSysDelete(std::string const& path, std::string const& serializedJSONValue)
     {
-        if (!isFrozen(BaSysID::getAddress(path))) {
-            auto deserialized = basyx::serialization::json::deserialize(serializedJSONValue);
-            providerBackend->deleteValue(path, std::move(deserialized));
-            incrementClock(BaSysID::getAddress(path));
-        }
+        auto deserialized = basyx::serialization::json::deserialize(serializedJSONValue);
+        providerBackend->deleteValue(path, std::move(deserialized));
 
         return serializeSuccess();
     }
 
     std::string processBaSysDelete(std::string const& path)
     {
-        if (!isFrozen(BaSysID::getAddress(path))) {
-            providerBackend->deleteValue(path);
-            incrementClock(BaSysID::getAddress(path));
-        }
-
+        providerBackend->deleteValue(path);
         return serializeSuccess();
     }
 
     std::string processBaSysInvoke(std::string const& path, std::string const& serializedJSONValue, char* output, size_t* size)
     {
 		auto deserialized = basyx::serialization::json::deserialize(serializedJSONValue);
-
 		auto res = providerBackend->invokeOperation(path, deserialized);
-
 		return serializeToJSON(path, res);
-
-//        if (deserialized.InstanceOf<basyx::object::object_list_t>()) {
-//            //auto& parameters = deserialized.Get<basyx::object::object_list_t&>();
-//			
-//            //TODO: res = providerBackend->invokeOperationImpl(path, parameters);
-//        } else {
-////TODO:            res = providerBackend->invokeOperation(path, deserialized);
-//        }
-
     }
 
 private:
@@ -112,29 +91,6 @@ private:
         nlohmann::json retJson { { "success", true }, { "entity", json } };
 
         return retJson.dump(4);
-    }
-
-    void incrementClock(std::string const& submodelPath)
-    {
-        std::string clockPath = submodelPath + "/properties/clock";
-        basyx::object clock = providerBackend->getModelPropertyValue(clockPath);
-
-		if (clock.IsNull()) {
-			providerBackend->createValue(clockPath, 1);
-		}
-		else {
-			providerBackend->setModelPropertyValue(clockPath, clock.Get<int>() + 1);
-		}
-    }
-
-    bool isFrozen(std::string const& submodelPath)
-    {
-        basyx::object modelPropertyValue = providerBackend->getModelPropertyValue(submodelPath + "/properties/frozen");
-
-        if (modelPropertyValue.IsNull())
-            return false;
-
-        return providerBackend->getModelPropertyValue(submodelPath + "/properties/frozen").template Get<bool>();
     }
 };
 
