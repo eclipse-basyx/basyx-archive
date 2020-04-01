8b127086ba6763b235e0e30a7fe5538cc5f522dc
Frank Schnicke <frank.schnicke@iese.fraunhofer.de>
2020-04-01 12:32:20 +0200

    Adds test for URL edge case not ending with /
    - When accessing the root, having no ending slash would crash the HTTP server
    
    Change-Id: I576f4b33e3c40366852e0cf6f81d02269166fc6e
    Signed-off-by: Frank Schnicke <frank.schnicke@iese.fraunhofer.de>

--- sdks/java/basys.sdk/src/test/java/org/eclipse/basyx/testsuite/regression/vab/protocol/http/TestVABHTTP.java
+++ sdks/java/basys.sdk/src/test/java/org/eclipse/basyx/testsuite/regression/vab/protocol/http/TestVABHTTP.java
@@ -1,10 +1,17 @@
 package org.eclipse.basyx.testsuite.regression.vab.protocol.http;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
 import org.eclipse.basyx.testsuite.regression.vab.modelprovider.TestProvider;
 import org.eclipse.basyx.vab.manager.VABConnectionManager;
 import org.eclipse.basyx.vab.protocol.http.connector.HTTPConnectorProvider;
 import org.eclipse.basyx.vab.protocol.http.server.BaSyxContext;
 import org.junit.Rule;
+import org.junit.Test;
 
 /**
  * Test VAB using HTTP protocol. This is an integration test
@@ -29,4 +36,24 @@ public class TestVABHTTP extends TestProvider {
 		return connManager;
 	}
 
+	/**
+	 * Tests for URL with no ending slash when accessing the root element, e.g.
+	 * http://localhost:8080/basys.sdk/Testsuite/SimpleVAB <br />
+	 * The SDK ensures that each access ends with a <i>/</i>. However, browser
+	 * requests do not necessarily conform to this
+	 */
+	@Test
+	public void testRootURL() {
+		Client client = ClientBuilder.newClient();
+
+		// Called URL
+		WebTarget resource = client.target("http://localhost:8080/basys.sdk/Testsuite/SimpleVAB");
+
+		// Build request, set JSON encoding
+		Builder request = resource.request();
+		request.accept(MediaType.APPLICATION_JSON);
+
+		// Perform request, no exception should be thrown here
+		request.get(String.class);
+	}
 }
