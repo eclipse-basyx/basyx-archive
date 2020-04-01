8dcb79c67df43125379863dde1384603633024e3
Frank Schnicke <frank.schnicke@iese.fraunhofer.de>
2020-04-01 13:48:34 +0200

    Refactors AASX component
    - Has explicit *Component class
    
    Change-Id: I26be3f34dd80dadc797c4b368eebd82c50c52c2d
    Signed-off-by: Frank Schnicke <frank.schnicke@iese.fraunhofer.de>

--- components/basys.components/basyx.components.docker/basyx.components.AASX/src/main/java/org/eclipse/basyx/components/executable/AASXExecutable.java
+++ components/basys.components/basyx.components.docker/basyx.components.AASX/src/main/java/org/eclipse/basyx/components/executable/AASXExecutable.java
@@ -4,10 +4,8 @@ import java.io.IOException;
 
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.eclipse.basyx.components.AASXComponent;
 import org.eclipse.basyx.components.configuration.BaSyxContextConfiguration;
-import org.eclipse.basyx.components.servlets.AASXAASServlet;
-import org.eclipse.basyx.vab.protocol.http.server.AASHTTPServer;
-import org.eclipse.basyx.vab.protocol.http.server.BaSyxContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xml.sax.SAXException;
@@ -29,12 +27,6 @@ public class AASXExecutable {
 	// Creates a Logger based on the current class
 	private static Logger logger = LoggerFactory.getLogger(AASXExecutable.class);
 
-	// The path the created servlet is mapped to
-	public static final String SERVLET_MAPPING = "/*";
-
-	// The server with the servlet that will be created
-	private static AASHTTPServer server;
-
 	public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
 		logger.info("Starting BaSyx AASX component");
 
@@ -42,36 +34,7 @@ public class AASXExecutable {
 		BaSyxContextConfiguration config = new BaSyxContextConfiguration();
 		config.loadFromResource(BaSyxContextConfiguration.DEFAULT_CONFIG_PATH);
 
-		startComponent(config.getHostname(), config.getPort(), config.getContextPath(), config.getDocBasePath(), config.getProperty("aasxPath"));
+		AASXComponent component = new AASXComponent(config.getHostname(), config.getPort(), config.getContextPath(), config.getDocBasePath(), config.getProperty("aasxPath"));
+		component.startComponent();
 	}
-
-	/**
-	 * Starts the AASX component at http://${hostName}:${port}/${path}
-	 * 
-	 * @param hostName
-	 * @param port
-	 * @param path
-	 * @param docBasePath
-	 * @throws IOException
-	 * @throws SAXException
-	 * @throws ParserConfigurationException
-	 */
-	public static void startComponent(String hostName, int port, String path, String docBasePath, String aasxPath) throws ParserConfigurationException, SAXException, IOException {
-		// Init HTTP context and add an XMLAAServlet according to the configuration
-		BaSyxContext context = new BaSyxContext(path, docBasePath, hostName, port);
-
-		// Create the Servlet for aas
-		context.addServletMapping(SERVLET_MAPPING, new AASXAASServlet(aasxPath));
-
-		// Create and start server
-		server = new AASHTTPServer(context);
-
-		logger.info("Starting server...");
-
-		// start the server
-		server.start();
-	}
-
-
-
 }
