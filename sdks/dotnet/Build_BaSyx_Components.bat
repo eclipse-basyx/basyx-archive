eccac3cee7dcae173f5f5bb9e8626ced761ea51a
Constantin Ziesche <constantin.ziesche@bosch.com>
2020-11-06 10:34:03 +0100

    Applications added

--- sdks/dotnet/Build_BaSyx_Components.bat
+++ sdks/dotnet/Build_BaSyx_Components.bat
@@ -1,4 +1,4 @@
 start /w Clear_Local_BaSyx_NuGet_Cache.bat
 
-dotnet build basyx-core\BaSyx.Core.sln --force
+dotnet build basyx-components\BaSyx.Components.sln --force
 IF %ERRORLEVEL% EQU 0 (exit 0) ELSE (Echo One or more errors occured during compiling)
--- sdks/dotnet/Build_BaSyx_Core_Components.bat
+++
@@ -1,9 +0,0 @@
-start /w Clear_Local_BaSyx_NuGet_Cache.bat
-
-dotnet build basyx-core\BaSyx.Core.sln --force
-
-start /w Clear_Local_BaSyx_NuGet_Cache.bat
-
-dotnet build basyx-components\BaSyx.Components.sln --force
-
-start /w Clear_Local_BaSyx_NuGet_Cache.bat
