922c782b93e43e2f5942f20902ccdf34a7a41fc1
Frank Schnicke <frank.schnicke@iese.fraunhofer.de>
2021-02-10 15:10:58 +0100

    Removes warning
    Change-Id: Ib4659654d93e4d8a1e7d53ce69b3a7edf7275b97
    Signed-off-by: Frank Schnicke <frank.schnicke@iese.fraunhofer.de>

--- sdks/java/basys.sdk/src/test/java/org/eclipse/basyx/testsuite/regression/aas/factory/xml/TestXMLConverter.java
+++ sdks/java/basys.sdk/src/test/java/org/eclipse/basyx/testsuite/regression/aas/factory/xml/TestXMLConverter.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.io.FileWriter;
 import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
