8816c7fdb68a5c732a059615de1342941a063389
Rene-Pascal Fischer <rene-pascal.fischer@iese.fraunhofer.de>
2021-08-20 15:18:23 +0200

    Fixes wrong XML id name creation
    * Adds test to check for proper ids
    
    Signed-off-by: Rene-Pascal Fischer <rene-pascal.fischer@iese.fraunhofer.de>
    Change-Id: Ic59aff9675ae16b845c397c550b50535450168cd

--- sdks/java/basys.sdk/src/test/java/org/eclipse/basyx/testsuite/regression/aas/factory/aasx/TestAASXToMetamodelConverterFromBaSyx.java
+++ sdks/java/basys.sdk/src/test/java/org/eclipse/basyx/testsuite/regression/aas/factory/aasx/TestAASXToMetamodelConverterFromBaSyx.java
@@ -13,16 +13,20 @@ package org.eclipse.basyx.testsuite.regression.aas.factory.aasx;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.eclipse.basyx.aas.bundle.AASBundle;
 import org.eclipse.basyx.aas.factory.aasx.AASXToMetamodelConverter;
@@ -70,6 +74,14 @@ public class TestAASXToMetamodelConverterFromBaSyx {
 	private static final String PDF_IDSHORT = "pdf";
 	private static final String TARGET_PATH = "target/files"; // gets set by BaSyx
 	private static final String[] EXPECTED_UNZIPPED_FILES = { TARGET_PATH + PDF_PATH, TARGET_PATH + IMAGE_PATH };
+
+	private static final String REL_PATH = "_rels/.rels";
+	private static final String ORIGIN_REL_PATH = "aasx/_rels/aasx-origin.rels";
+
+	private static final String ID_REGEX_START = "Id=.*";
+	// only letters or underscore as start of id allowed
+	// https://www.w3.org/TR/1999/REC-xml-names-19990114/#ns-qualnames
+	private static final String ID_REGEX_FULL = "Id=\"([A-z]|_).*";
 
 	private int bundleSize;
 	private int submodelSize;
@@ -129,6 +141,115 @@ public class TestAASXToMetamodelConverterFromBaSyx {
 		for (String path : EXPECTED_UNZIPPED_FILES) {
 			assertTrue(new java.io.File(path).exists());
 		}
+	}
+
+	/**
+	 * Tests if the ids start with a letter or underscore.
+	 * 
+	 * @throws IOException
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 */
+	@Test
+	public void testRelationshipsOfGeneratedAASX() throws IOException {
+		ZipInputStream in = unzipFromPath(CREATED_AASX_FILE_PATH);
+
+		String relString = getFileContentFromZipInputStream(REL_PATH, in);
+		String originRelString = getFileContentFromZipInputStream(ORIGIN_REL_PATH, in);
+
+		List<String> relStringIds = findIdCandidates(relString);
+		List<String> originRelStringIds = findIdCandidates(originRelString);
+
+		assertIdsBeginWithLetterOrUnderscore(relStringIds);
+		assertIdsBeginWithLetterOrUnderscore(originRelStringIds);
+	}
+
+	/**
+	 * Check if elements of the given id list adhere to the id regEx.
+	 * 
+	 * @param idStringList
+	 */
+	private void assertIdsBeginWithLetterOrUnderscore(List<String> idStringList) {
+		assertStringListRegExCheck(idStringList, ID_REGEX_FULL);
+	}
+
+	/**
+	 * Split the given string at every whitespace and returns all elements that
+	 * start with "Id="
+	 * 
+	 * @param stringToCheck
+	 * @return List<String>
+	 */
+	private List<String> findIdCandidates(String stringToCheck) {
+		return findRegExParts(stringToCheck, ID_REGEX_START);
+	}
+
+	/**
+	 * Check if elements of the given list adhere to the given regEx.
+	 * 
+	 * @param stringToCheck
+	 * @param regEx
+	 */
+	private void assertStringListRegExCheck(List<String> stringToCheck, String regEx) {
+		for (String part : stringToCheck) {
+			assertTrue(part.matches(regEx));
+		}
+	}
+
+	/**
+	 * Split the given string at every whitespace and returns a list of all
+	 * remaining elements that adhere to the given regEx.
+	 * 
+	 * @param stringToCheck
+	 * @param regEx
+	 * @return List<String>
+	 */
+	private List<String> findRegExParts(String stringToCheck, String regEx) {
+		String[] stringParts = stringToCheck.split(" ");
+		List<String> regExParts = new ArrayList<String>();
+
+		for (String part : stringParts) {
+			if (part.matches(regEx)) {
+				regExParts.add(part);
+			}
+		}
+
+		return regExParts;
+	}
+
+	/**
+	 * Get the content of a file with a given path from a ZipInputStream as String
+	 * 
+	 * @param filePath
+	 * @param in
+	 * @return String of the file contents
+	 * @throws IOException
+	 */
+	private String getFileContentFromZipInputStream(String filePath, ZipInputStream in) throws IOException {
+		ZipEntry zipEntry = null;
+		String contentString = "";
+
+		while ((zipEntry = in.getNextEntry()) != null) {
+			if (zipEntry.getName().equals(filePath)) {
+				byte[] buf = new byte[1];
+
+				while (in.read(buf) > 0) {
+					contentString += new String(buf);
+				}
+			}
+		}
+		return contentString;
+	}
+
+	/**
+	 * Unzip a file from path using a ByteArrayInputStream
+	 * 
+	 * @param path
+	 * @return ZipInputStream
+	 * @throws IOException
+	 */
+	private ZipInputStream unzipFromPath(String path) throws IOException {
+		return new ZipInputStream(new ByteArrayInputStream(FileUtils.readFileToByteArray(new java.io.File(path))));
 	}
 
 	/**
