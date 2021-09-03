302db2ecfc63a0204ed150fce0b9ec39333d4ba7
Frank Schnicke <frank.schnicke@iese.fraunhofer.de>
2021-09-03 16:00:33 +0200

    Fixes wrong passed path for mongodb property files
    Change-Id: Iee16c425023d41b2ba1783c87e06ae2bd86cea68
    Signed-off-by: Frank Schnicke <frank.schnicke@iese.fraunhofer.de>

--- components/basys.components/basyx.components.docker/basyx.components.AASServer/src/test/java/org/eclipse/basyx/regression/AASServer/TestMongoDBAggregator.java
+++ components/basys.components/basyx.components.docker/basyx.components.AASServer/src/test/java/org/eclipse/basyx/regression/AASServer/TestMongoDBAggregator.java
@@ -11,14 +11,14 @@ package org.eclipse.basyx.regression.AASServer;
 
 import org.eclipse.basyx.aas.aggregator.api.IAASAggregator;
 import org.eclipse.basyx.components.aas.mongodb.MongoDBAASAggregator;
-import org.eclipse.basyx.components.configuration.BaSyxContextConfiguration;
+import org.eclipse.basyx.components.configuration.BaSyxMongoDBConfiguration;
 import org.eclipse.basyx.testsuite.regression.aas.aggregator.AASAggregatorSuite;
 
 public class TestMongoDBAggregator extends AASAggregatorSuite {
 
 	@Override
 	protected IAASAggregator getAggregator() {
-		MongoDBAASAggregator aggregator = new MongoDBAASAggregator(BaSyxContextConfiguration.DEFAULT_CONFIG_PATH);
+		MongoDBAASAggregator aggregator = new MongoDBAASAggregator(BaSyxMongoDBConfiguration.DEFAULT_CONFIG_PATH);
 		aggregator.reset();
 
 		return aggregator;
